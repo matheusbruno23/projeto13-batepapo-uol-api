594cdf0b79bafe026ba908e29baa9ad9ea0a88f8
matheusbruno23 <matheus.bruno.1805@gmail.com>
2023-04-12 22:36:20 -0300

    retorna lista de participantes e array vazio caso não haja participantes cadastrados

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import {MongoClient, ObjectId} from "mongodb"
+import { MongoClient, ObjectId } from "mongodb"
 import dotenv from 'dotenv';
 import Joi from "joi";
 
@@ -16,56 +16,73 @@ let db
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 mongoClient.connect()
     .then(() => db = mongoClient.db())
-    .catch(err => console.log(err.message)) 
+    .catch(err => console.log(err.message))
 
-    app.post("/participants", async (req, res) => {
-        const { name } = req.body;
-      
-        const participantSchema = Joi.object({
-          name: Joi.string().required().exist({
+app.post("/participants", async (req, res) => {
+
+    const { name } = req.body;
+    //Validação de usuário pelo Joi
+    const participantSchema = Joi.object({
+        name: Joi.string().required().exist({
             db: db,
             collection: "participants",
             fieldName: "name"
-          })
-        });
-      
-        const validation = participantSchema.validate(req.body);
-      
-        if (validation.error) {
-          // Retorna 422 caso o nome não seja uma string ou seja vazio
-          return res.status(422).send(validation.error.details[0].message);
-        }
-      
-        try {
-          // Verifica se o nome já existe no banco de dados
-          const participant = await db
+        })
+    });
+    //valida o nome
+    const validation = participantSchema.validate(req.body);
+
+    if (validation.error) {
+        // Retorna 422 caso o nome não seja uma string ou seja vazio
+        return res.status(422).send(validation.error.details[0].message);
+    }
+
+    try {
+        // Verifica se o nome já existe no banco de dados
+        const participant = await db
             .collection("participants")
             .findOne({ name: name });
-      
-          if (participant) {
+
+        if (participant) {
             // Retorna 409 caso o nome já exista no banco de dados
             return res.sendStatus(409);
-          }
-      
-          // Cadastra o participante no banco de dados
-          const user = {
+        }
+
+        // Cadastra o participante no banco de dados
+        const user = {
             name: name,
             lastStatus: Date.now()
-          };
-      
-          const result = await db.collection("participants").insertOne(user);
-          const time = new Date(user.lastStatus).toLocaleTimeString("pt-BR");
-          const message = {
-            from: user.name,
+        };
+
+        const result = await db.collection("participants").insertOne(user);
+        // Formata o horário para a mensagem de entrada
+        const time = new Date(user.lastStatus).toLocaleTimeString("pt-BR");
+        const message = {
+            from: name,
             to: "Todos",
             text: "entra na sala...",
             type: "status",
             time: time
-          };
-          await db.collection("messages").insertOne(message);
-          res.sendStatus(201);
-        } catch (err) {
-          res.status(500).send(err.message);
-        }
-      });
+        };
+
+        //Envia mensagem automática no chat
+        await db.collection("messages").insertOne(message);
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.get("/participants", async (req, res) => {
+    try {
+      const size = await db.collection("participants").countDocuments({});
+      if (size === 0) {
+        return [];
+      }
+      const users = await db.collection("participants").find().toArray();
+      res.send(users);
+    } catch (err) {
+      res.send(err.message);
+    }
+  });
 app.listen(5000)
