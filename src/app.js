7f6040f88b0d5a50d25235c3fcdecfbedb5eb773
matheusbruno23 <matheus.bruno.1805@gmail.com>
2023-04-14 16:43:31 -0300

    teste de debug

--- src/app.js
+++ src/app.js
@@ -80,7 +80,7 @@ app.get("/participants", async (req, res) => {
       const size = await db.collection("participants").countDocuments({});
       //retorna um array vazio caso nenhum usuário esteja cadastrado
       if (size === 0) {
-        return [];
+        return res.send();
       }
       const users = await db.collection("participants").find().toArray();
       res.send(users);
@@ -91,7 +91,7 @@ app.get("/participants", async (req, res) => {
 
 app.post("/messages", async (req , res) => {
     const {to , text, type} = req.body
-    const from = req.header("User")
+    const from = req.headers.user
     if(!from){
         return res.sendStatus(422)
     }
@@ -128,7 +128,7 @@ app.post("/messages", async (req , res) => {
 })
 
 app.get("/messages", async (req, res) => {
-    const user = req.header.user
+    const user = req.headers.user
 
     const limit = Number(req.query.limit)
     if (isNaN(limit) || limit <= 0) {
@@ -172,25 +172,24 @@ app.post('/status', async (req, res) => {
 
 
   //removendo usuários afk
-// setInterval(async () =>{
+//   setInterval(async () => {
 //     const now = Date.now()
 //     const limite = now - 10000
-//     const afks = await db.collection("participants").findMany({lastStatus: {$lt: limite}}).toArray()
-    
-//     if(afks.deletedCount > 0){
-//         afks.forEach(async (participant) => {
-//             const message = {
-//                 from: participant.name,
-//                 to:"Todos",
-//                 text:"sai da sala...",
-//                 type:"status",
-//                 time: dayjs().format("HH:mm:ss")
-//             }
-//             await db.collection("messages").insertOne(message)
-//             await db.collection("participants").remove({participant})
-//         })
+//     const afks = await db.collection("participants").findMany({ lastStatus: { $lt: limite } }).toArray()
+  
+//     if (afks.length > 0) {
+//       afks.forEach(async (participant) => {
+//         const message = {
+//           from: participant.name,
+//           to: "Todos",
+//           text: "sai da sala...",
+//           type: "status",
+//           time: dayjs().format("HH:mm:ss")
+//         }
+//         await db.collection("messages").insertOne(message)
+//         await db.collection("participants").deleteMany({_id: {$in: afks.map(p => p._id)}})
+//       })
 //     }
-
 //   }, 15000)
 
 app.listen(5000)
